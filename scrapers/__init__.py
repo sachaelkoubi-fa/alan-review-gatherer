cbb9ff186c7c728a0d3189e51304f72a83957849
sachaelkoubi-fa <sacha.elkoubi1@gmail.com>
2026-02-18 10:24:51 +0100

    Fix __init__.py: replace appfigures import with appstore_reviews

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -2,4 +2,4 @@ from .base import Review
 from .trustpilot_reviews import scrape_trustpilot
 from .opinion_assurances_reviews import scrape_opinion_assurances
 from .google_reviews import scrape_google_reviews
-from .appfigures_reviews import scrape_appfigures
+from .appstore_reviews import scrape_app_store_reviews
